3f01b491e539b3078f9577f16655e86b29506176
Divyendu5904 <129673673+Divyendu5904@users.noreply.github.com>
2024-06-18 14:14:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all jJS code as newer version
+
+
+//number = 2 to the power 53 range
+//bigint
+//string =""
+//boolean=true/false
+//null = stand alone value
+//undefined
+//symbol= unique
+
+
+
+//object
+
+
+console.log(typeof "divyam")
+console.log(typeof "null")//object
+console.log(typeof "undefined")//undefined

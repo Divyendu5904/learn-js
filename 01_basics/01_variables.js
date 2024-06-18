7eb779c3a3ebbdd737e3f7b5893ce49901b0757f
Divyendu5904 <129673673+Divyendu5904@users.noreply.github.com>
2024-06-18 14:15:46 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail= "divyam@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+//accountId chnage not allowed due to constant keyword
+
+accountEmail="div@gmail.com"
+accountPassword="124124"
+accountCity="delhi"
+
+console.table([accountEmail, accountPassword, accountPassword, accountState])

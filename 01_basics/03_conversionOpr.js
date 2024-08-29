b74738822dd1b7a8771d29143fe8ce2d33d4e722
Divyendu5904 <129673673+Divyendu5904@users.noreply.github.com>
2024-08-29 20:17:22 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOpr.js
@@ -0,0 +1,26 @@
+let score="33"
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=> 33
+// "33abc"=>NaN------ not a number 
+// true=>1; false=>0
+
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//""=> false  -- empty
+//"any value"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);

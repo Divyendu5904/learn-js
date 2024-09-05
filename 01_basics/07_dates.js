5a1135c0119aecb2d3c4789336244abf68fee695
Divyendu5904 <129673673+Divyendu5904@users.noreply.github.com>
2024-09-05 18:17:01 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,16 @@
+//dates
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+console.log(typeof myDate)
+
+let myCreatedDate= new Date(2024,0,24)
+console.log(myCreatedDate)
+
+
+console.log()
+
+console.log(myCreatedDate.toLocaleTimeString())

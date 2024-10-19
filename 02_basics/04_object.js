42f209244ed0ab224d59c5310ae613cd978e5fef
Divyendu5904 <129673673+Divyendu5904@users.noreply.github.com>
2024-10-19 11:19:36 +0000

    json

---
+++ 02_basics/04_object.js
@@ -0,0 +1,18 @@
+const course = {
+    courseName:" js in hindi",
+    price:"999",
+    courseLearner:"divyam"
+}
+
+// course.courseLearner
+
+const{courseLearner : Learner} = course
+// console.log(courseLearner)
+console.log(Learner)
+
+
+// const navbar = ({company}) =>{
+
+// }             // object destructuring
+
+// navbar(company = "omnitrix")
--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,4 +17,11 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullName.use)
+// console.log(regularUser.fullName.userfullname)
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+// const obj3 = Object.assign({},obj1, obj2)
+const obj3 = {...obj1,...obj2}
+console.log(obj3)
